6b9736d2f3069297eb263dd6a716c9857e078f03
murielguedes <murielguedes@proton.me>
2022-11-15 15:23:38 -0300

    .

--- rust-server-auth/src/static_files.rs
+++
@@ -1,9 +0,0 @@
-pub fn send_static_file(req: &mut gondor_io::Request) -> Result<(),std::io::Error> {
-    let path = req.path();
-    match path {
-        b"main" | b"css/main.css" => req.send(200, "text/css", include_bytes!("../public/css/main.css")),
-		b"" | b"index" | b"index.html" => req.send(200, "text/html", include_bytes!("../public/index.html")),
-		b"ok" | b"ok.txt" => req.send(200, "text/plain", include_bytes!("../public/ok.txt")),
-		_ => Ok(())
-    }
-}
--- rust-server-auth/tests/run.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-cargo build --release
-powershell -Command "cp ./target/release/rust-server-auth.exe ./tests/"
-start cmd /c "cd tests & rust-server-auth.exe"
-pause
-cargo test --release -- --show-output
